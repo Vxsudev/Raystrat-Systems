57e930e4f55f383073525241f6d72c5bdeae4ee3
Vasudeva Rao <vp@raystrat.com>
2025-08-19 16:11:42 +0000

    make it 2x faster - its still slow

--- src/components/ui/marquee.tsx
+++ src/components/ui/marquee.tsx
@@ -37,7 +37,7 @@ export function Marquee() {
           }
         }
         .animate-marquee {
-          animation: marquee 8s linear infinite;
+          animation: marquee 4s linear infinite;
         }
       `}</style>
     </div>
