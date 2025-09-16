b50791e79fe0a56f6abd7ae2bbca8f0f7003a41f
Vasudeva Rao <vp@raystrat.com>
2025-09-16 21:14:04 +0000

    upload as PNG not SVG

--- src/components/ui/logo-icon.tsx
+++ src/components/ui/logo-icon.tsx
@@ -9,14 +9,14 @@ export function LogoIcon({ className }: { className?: string }) {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-        <path 
-            d="M5.33331 20L11.3333 4H15.3333L10.6666 16H16.6666L19.3333 20H5.33331Z" 
-            fill="white"
-        />
-        <path 
-            d="M16.6667 16L15.3334 12L19.3334 4H22.6667L16.6667 16Z"
-            className="fill-primary"
-        />
+      <path 
+          d="M5.33331 20L11.3333 4H15.3333L10.6666 16H16.6666L19.3333 20H5.33331Z" 
+          fill="currentColor"
+      />
+      <path 
+          d="M16.6667 16L15.3334 12L19.3334 4H22.6667L16.6667 16Z"
+          className="fill-primary"
+      />
     </svg>
   );
 }
