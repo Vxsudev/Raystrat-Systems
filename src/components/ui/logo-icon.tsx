753d02586f69126b52ba2d8e8b883d632b94e431
Vasudeva Rao <vp@raystrat.com>
2025-09-16 21:22:08 +0000

    viewBox="0 0 100 100"

--- src/components/ui/logo-icon.tsx
+++ src/components/ui/logo-icon.tsx
@@ -5,16 +5,16 @@ export function LogoIcon({ className }: { className?: string }) {
   return (
     <svg
       className={cn("text-foreground", className)}
-      viewBox="0 0 80 80"
+      viewBox="0 0 100 100"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path 
-        d="M20,66 L20,20.4 L31.2,14 L31.2,54.8 L60,54.8 L60,66 L20,66 Z" 
+      <path
+        d="M20 78L20 22.4L31.2 16L31.2 66.8L72 66.8L72 78H20Z"
         fill="currentColor"
       />
-      <path 
-        d="M36.4,49.6 L60,26 L60,14 L36.4,37.6 L36.4,49.6 Z" 
+      <path
+        d="M36.4 61.6L72 38L72 26L36.4 49.6V61.6Z"
         className="fill-primary"
       />
     </svg>
