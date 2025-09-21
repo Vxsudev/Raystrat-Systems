a35787992494609b846ab54d263b9295103be15c
Vasudeva Rao <vp@raystrat.com>
2025-09-21 19:21:05 +0000

    i dont think colum layout or whatever is the issue - check again - think

--- next.config.ts
+++ next.config.ts
@@ -24,19 +24,6 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'x-next-pathname',
-            value: ':path*',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
