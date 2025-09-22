9283b7e3ba87d4df733f195bd472b41c1e4d854a
Vasudeva Rao <vp@raystrat.com>
2025-09-22 06:46:08 +0000

    whats blocking you from rendering my dashboard image on my hero section?

---
+++ src/next.config.ts
@@ -0,0 +1,35 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  // This ensures that API routes are not processed at build time.
+  // It prevents errors when routes require runtime environment variables.
+  experimental: {
+    instrumentationHook: true, 
+  },
+  output: 'standalone',
+};
+
+export default nextConfig;
