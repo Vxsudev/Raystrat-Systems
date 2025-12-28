856014b7222680f8e29d5173a9ffd398a56795d3
Vasudeva Rao <vp@raystrat.com>
2025-12-28 16:00:36 +0000

    make the homepage backrounds dots little dimmer

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'dotted-pattern': 'radial-gradient(circle at 1px 1px, hsl(var(--foreground) / 0.2) 1px, transparent 0)',
+        'dotted-pattern': 'radial-gradient(circle at 1px 1px, hsl(var(--foreground) / 0.1) 1px, transparent 0)',
       },
       backgroundSize: {
         'dotted-pattern': '16px 16px',
